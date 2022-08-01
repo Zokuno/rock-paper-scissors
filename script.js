86290b987970e97890fa24b2a878a8f7f95831d3
Zokuno <61715120+Zokuno@users.noreply.github.com>
2022-08-01 21:48:43 +1000

    Update script.js
    Changed code in playRound to utilize OR statements.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/* // tips: use return mostly, use console.log() to display results/winner of each round. Use prompt() to get input from the user. Might be helpful to change return value to something more useful */
 function getComputerChoice() {
     const choices = ['ROCK', 'PAPER', 'SCISSORS'];
     let random = Math.floor(Math.random() * choices.length);
@@ -13,31 +12,20 @@ function playRound(playerSelection, computerSelection) {
     let loser = "You Lose!" + " " + computerSelection + " " + "beats" + " " + playerSelection;
     let winner = "You Win!" + " " + playerSelection + " " + "beats" + " " + computerSelection;
     let draw = "Draw! No Winner";
-    if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
+    if (playerSelection === 'ROCK' && computerSelection === 'PAPER' || playerSelection === 'PAPER' && computerSelection === 'SCISSORS' || playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
         return loser;
-    }   else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
-            return winner;
-    }   else if (playerSelection === 'ROCK' && computerSelection === 'ROCK') {
-            return draw;
-    }    else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
-        return loser;
-    }   else if (playerSelection === 'PAPER' &&        computerSelection === 'ROCK') {
-            return winner;
-    }   else if (playerSelection === 'PAPER' && computerSelection === 'PAPER') {
-            return draw;
-    }   else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
-        return loser;
-    }    else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
-            return winner;
-    }    else if (playerSelection === 'SCISSORS' && computerSelection === 'SCISSORS') {
-            return draw;
-    }    else {
-            return "Please type 'Rock', 'Paper', or 'Scissors'";
-        }
+    }   else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS' || playerSelection === 'PAPER' && computerSelection === 'ROCK' || playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+        return winner;
+    }   else if (playerSelection === 'ROCK' && computerSelection === 'ROCK' || playerSelection === 'PAPER' && computerSelection === 'PAPER' || playerSelection === 'SCISSORS' && computerSelection === 'SCISSORS') { 
+        return draw
+    }   else  {
+        return "Please type 'Rock', 'Paper', or 'Scissors'";
+    }
 }
-
 function game() {
-            // this calls playRound function to play a 5 round game that keeps score and reports a winner or loser at the end. Utilise loops.
+    for (let i = 0; i < 5; i++) {
+        // your code here!
+    }      
 }
 
 console.log(playRound());
