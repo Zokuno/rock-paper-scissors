4b719bfae884090dbf657f3718063c5711a43797
Zokuno <joel.brookes@gmail.com>
2022-09-09 21:48:55 +1000

    Everything works except reset

--- script.js
+++ script.js
@@ -11,22 +11,21 @@ let computerScore = 0;
 
 // Takes player text as input and compares to computerSelection to play a single round of Rock, Paper, Scisscors. Adds +1 to either playerScore or computerScore depending on who wins the round
 function playRound(playerSelection, computerSelection) {
-    console.log("You chose" + " " + playerSelection);
-    console.log("Opponent chose" + " " + computerSelection);
+    let p1Selection = "You chose" + " " + playerSelection;
+    let p2Selection = "Opponent chose" + " " + computerSelection;
     let loser = "You Lose!" + " " + computerSelection + " " + "beats" + " " + playerSelection + "!";
     let winner = "You Win!" + " " + playerSelection + " " + "beats" + " " + computerSelection + "!";
     let draw = "Draw! No Winner";
+    p1Choice.textContent = p1Selection;
+    p2Choice.textContent = p2Selection;
     if (playerSelection === 'Rock' && computerSelection === 'Paper' || playerSelection === 'Paper' && computerSelection === 'Scissors' || playerSelection === 'Scissors' && computerSelection === 'Rock') {
         computerScore++;
-        console.log(loser);
-        gameStatus.textContent = loser;
+        gameOutcome.textContent = loser;
     }   else if (playerSelection === 'Rock' && computerSelection === 'Scissors' || playerSelection === 'Paper' && computerSelection === 'Rock' || playerSelection ===       'Scissors' && computerSelection === 'Paper') {
         playerScore++;
-        console.log(winner);
-        gameStatus.textContent = winner;
+        gameOutcome.textContent = winner;
     }   else if (playerSelection === 'Rock' && computerSelection === 'Rock' || playerSelection === 'Paper' && computerSelection === 'Paper' || playerSelection === 'Scissors' && computerSelection === 'Scissors') { 
-        console.log(draw);
-        gameStatus.textContent = draw;
+        gameOutcome.textContent = draw;
     }   else  {
         console.log("No Contest! Please click 'Rock', 'Paper', or 'Scissors'");
     }
@@ -34,7 +33,9 @@ function playRound(playerSelection, computerSelection) {
     p2Display.textContent = computerScore;
 }
 
-let gameStatus = document.querySelector(".gameStatus");
+let p1Choice = document.querySelector(".p1Choice");
+let p2Choice = document.querySelector(".p2Choice");
+let gameOutcome = document.querySelector(".gameOutcome");
 let rockBtn = document.querySelector(".btn1");
 let paperBtn = document.querySelector(".btn2");
 let scissorsBtn = document.querySelector(".btn3");
